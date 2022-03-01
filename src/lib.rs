e982c00fefb8db79b07102be22755253fc6249d1
Gerry <gerry@B-4V0MWKCJ-2052.local>
2022-03-01 11:35:34 +0800

    Refine error messages
    Fix grammer errors in error messages.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- src/lib.rs
+++ src/lib.rs
@@ -48,33 +48,33 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("create netns dir failed. {0}")]
+    #[error("Can not create netns directory, {0}")]
     CreateNsDirError(std::io::Error),
 
-    #[error("create netns failed. {0}")]
+    #[error("Can not create netns,{0}")]
     CreateNsError(std::io::Error),
 
-    #[error("open netns {0} failed. {1}")]
+    #[error("Can not open netns {0},{1}")]
     OpenNsError(std::path::PathBuf, std::io::Error),
 
-    #[error("close netns failed. {0}")]
+    #[error("Failed to close netns,{0}")]
     CloseNsError(nix::Error),
 
-    #[error("remove netns {0} failed. {1}")]
+    #[error("Can not remove netns {0}, {1}")]
     RemoveNsError(std::path::PathBuf, std::io::Error),
 
-    #[error("mount {0} failed. {1}")]
+    #[error("Failed to mount {0}, {1}")]
     MountError(String, nix::Error),
 
-    #[error("unmount {0} failed. {1}")]
+    #[error("Failed to unmount {0}, {1}")]
     UnmountError(std::path::PathBuf, nix::Error),
 
-    #[error("unshare failed. {0}")]
+    #[error("Failed to unshare, {0}")]
     UnshareError(nix::Error),
 
-    #[error("join thread failed. {0}")]
+    #[error("Failed to join thread, {0}")]
     JoinThreadError(String),
 
-    #[error("setns failed. {0}")]
+    #[error("Can not setns, {0}")]
     SetnsError(nix::Error),
 }
